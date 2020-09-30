0f89c4f632d068d12bd99cca7b28906f71986d3d
cassymarie <cassy2205@gmail.com>
2020-09-30 13:05:07 -0600

    Learn.co IDE error, could not push to Repository.  Manually done here

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,11 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+# require 'pry'
+def meal_choice(veg1, veg2, protein = 'tofu')
+  puts "What a nutritious meal!"
+  puts choice = "A plate of #{protein} with #{veg1} and #{veg2}."
+  # binding.pry
+  return choice
+end
+
+meal_choice('carrots','string beans')
